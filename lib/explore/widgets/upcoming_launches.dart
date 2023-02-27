9692a352e3ec43229d888bfc257998523fe01031
ivirtex <hjozwiakdx@gmail.com>
2023-02-27 15:49:19 +0100

    Increase upcoming launches card height

--- lib/explore/widgets/upcoming_launches.dart
+++ lib/explore/widgets/upcoming_launches.dart
@@ -26,7 +26,7 @@ class UpcomingLaunches extends StatelessWidget {
       children: [
         const Section(name: 'Upcoming Launches'),
         SizedBox(
-          height: 110,
+          height: 115,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: launches.results!.length,
