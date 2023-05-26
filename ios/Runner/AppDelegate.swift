86592721b51be2fe625044fc8abca9e3dd0d1df8
ivirtex <hjozwiakdx@gmail.com>
2023-05-26 22:34:46 +0200

    Register launch check tasks

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,6 +10,10 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyCgiB7NYGvrW77rwJsKDyTsguxH8vPH_Eo")
+
+    WorkmanagerPlugin.registerTask(withIdentifier: "autoNextLaunchCheck")
+    WorkmanagerPlugin.registerTask(withIdentifier: "userSpecifiedLaunchCheck")
+    
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
