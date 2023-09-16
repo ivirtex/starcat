0fe972c33a8f000a8dd96e792f12b71b8c5eaec7
ivirtex <hjozwiakdx@gmail.com>
2023-09-17 00:37:36 +0200

    Setup push notifications on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,30 +1,16 @@
 import Flutter
 import GoogleMaps
 import UIKit
-import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    static func registerPlugins(with registry: FlutterPluginRegistry) {
-        GeneratedPluginRegistrant.register(with: registry)
-    }
-
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         GMSServices.provideAPIKey("AIzaSyCdg6Gj1eREabQ43KTxcq560aEm2JrCkTs")
 
-        WorkmanagerPlugin.registerTask(withIdentifier: "dev.ivirtex.starcat.autoNextLaunchCheck")
-        WorkmanagerPlugin.registerTask(withIdentifier: "dev.ivirtex.starcat.userSpecifiedCheck")
-
-        UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60 * 15))
-
-        AppDelegate.registerPlugins(with: self)
-        WorkmanagerPlugin.setPluginRegistrantCallback { registry in
-            AppDelegate.registerPlugins(with: registry)
-        }
-
+        GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
